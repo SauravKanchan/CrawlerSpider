7fc2ce7a106738927823736af972a3406052a8c2
SauravKanchan <sauravnk30@gmail.com>
2016-08-28 22:48:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-'''
-import threading
-from queue import Queue
-from spider import Spider
-from domain import *
-from general import *
-
-PROJECT_NAME = "SauravKanchan"
-HOMEPAGE =  "https://thenewboston.com/"
-DOMAIN_NAME = get_domain_name(HOMEPAGE)
-QUEUE_FILE = PROJECT_NAME + "/queue.txt"
-CRAWLED_FILE = PROJECT_NAME + "/crawled.txt"
-NUMBER_OF_THREADS = 8
-queue = Queue()
-Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
-'''
-
 import threading
 from queue import Queue
 from spider import Spider
